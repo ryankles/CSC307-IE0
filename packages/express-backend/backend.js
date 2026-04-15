f2a9189fbe04a1c19bcb86059e70ca81b0e2d47f
Ryan Reeves <ryanreevesyt@gmail.com>
2026-04-14 18:56:57 -0700

    Completed IE2

---
+++ packages/express-backend/backend.js
@@ -0,0 +1,117 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
+
+app.use(express.json());
+
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+const findUsersByNameAndJob = (name, job) => {
+  return users["users_list"].filter((user) => {
+    const matchesName = name === undefined || user["name"] === name;
+    const matchesJob = job === undefined || user["job"] === job;
+
+    return matchesName && matchesJob;
+  });
+};
+
+const deleteUserById = (id) => {
+  const userIndex = users["users_list"].findIndex((user) => user["id"] === id);
+
+  if (userIndex === -1) {
+    return false;
+  }
+
+  users["users_list"].splice(userIndex, 1);
+  return true;
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+  if (name != undefined || job != undefined) {
+    let result = findUsersByNameAndJob(name, job);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const wasDeleted = deleteUserById(id);
+
+  if (!wasDeleted) {
+    res.status(404).send("Resource not found.");
+    return;
+  }
+
+  res.status(204).send();
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
